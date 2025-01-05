63ce79691e356fad7b1502b274fb8277a53a06fb
Harm355 <151342662+Harm355@users.noreply.github.com>
2025-01-05 17:54:08 +0545

    antivirus
    it was created in sep 2023

---
+++ AntiVirus by Windows25 Developer.bat
@@ -0,0 +1,27 @@
+﻿@echo off
+title Antivirus Virus
+cls
+Echo Hello Do You Scan PC?
+Echo Please Enter Name
+Pause
+Cls
+set /p name=
+Echo Scanning...
+Pause
+Cls
+Dir/s
+Cls
+:infected
+Echo Warning Virus Detected!!
+Pause
+Cls
+if exist %name% //a
+Echo Deleting...
+Pause
+Cls
+:Del
+Del %name%
+Echo Enjoy Your PC!!
+Pause
+Cls
+Exit
